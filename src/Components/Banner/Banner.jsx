a12d3d1729650b5be15941b77dab35f7a24d9f23
Your Name <you@example.com>
2023-12-21 16:34:56 +0600

    banner added

---
+++ src/Components/Banner/Banner.jsx
@@ -0,0 +1,19 @@
+
+const Banner = () => {
+    return (
+            
+<div className="hero min-w-full min-h-screen relative" style={{backgroundImage: 'url(https://i.ibb.co/j48YC9t/Grey-Modern-Professional-Business-Project-Presentation.png)'}}>
+  <div className="absolute inset-0 flex items-center justify-center">
+    {/* <button
+      className="rounded-lg bg-gradient-to-r from-pink-500 to-purple-500 py-3.5 px-7 font-sans text-sm font-bold uppercase text-white shadow-md hover:shadow-lg focus:shadow-outline focus:outline-none transition duration-300"
+      data-ripple-light="true"
+    >
+      Explore More
+    </button> */}
+  </div>
+</div>
+
+    );
+};
+
+export default Banner;
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,10 @@
+import Banner from "../Banner/Banner";
 
 const Home = () => {
 
   return (
     <div>
-      
+      <Banner></Banner>
 This is Home
     </div>
   );
